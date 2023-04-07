b73e823300ce7ce20d32894b9abf989032a600ed
italo-morais <franciscoitalovianademorais@gmail.com>
2023-04-07 08:54:28 -0300

    create app

---
+++ client/src/App/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Header } from '../components/Header'
+import { Donation } from '../components/Donation'
+
+export function App() {
+  return (
+    <>
+    <header>
+      <Header />
+    </header>
+    <main>
+      <Donation />
+    </main>
+    </>
+  )
+}
+
+
